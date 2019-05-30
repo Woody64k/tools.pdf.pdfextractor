f116728cf6e157d27349a4dea501c460c7ced9c3
Sebastian Sauerbier <s.sauerbier@mid.de>
2019-05-30 08:39:32 +0200

    Analyze all PDF in a Folder.

--- src/main/java/de/woody64k/tools/pdf/pdfextractor/App.java
+++ src/main/java/de/woody64k/tools/pdf/pdfextractor/App.java
@@ -3,9 +3,14 @@ package de.woody64k.tools.pdf.pdfextractor;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.slf4j.Logger;
@@ -15,22 +20,29 @@ public class App {
 	private static Logger LOG = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) throws URISyntaxException, IOException {
-		final String address = "D:\\temp\\pdf\\PDF-Formular-Beispiel.pdf";
-		LOG.info("Print Load Document");
-		try {
-			// URL scalaByExampleUrl = new URL(address);
-			// PDDocument pdDoc = PDDocument.load(scalaByExampleUrl.openStream());
-			File pdfFile = new File(address);
-			PDDocument pdDoc = PDDocument.load(pdfFile);
-			PDDocumentCatalog pdCatalog = pdDoc.getDocumentCatalog();
-			PDAcroForm pdAcroForm = pdCatalog.getAcroForm();
+		final File workingDir = new File("C:\\temp\\pdf\\");
+		List<File> pdfFileList = Arrays.asList(workingDir.listFiles(a -> a.getName().endsWith(".pdf")));
 
-			for(PDField pdField : pdAcroForm.getFields()){
-				pdField.getFullyQualifiedName();
-				pdField.getValueAsString();
+		pdfFileList.forEach(pdfFile -> {
+			LOG.info("Print Load Document");
+			try {
+				Map<String, String> propertyMap = getFieldsFromPdfForm(pdfFile);
+				System.out.println(propertyMap);
+			} catch (IOException ioEx) {
+				LOG.error("Main-Error: ", ioEx);
 			}
-		} catch (IOException ioEx) {
-			LOG.error("Main-Error: ", ioEx);
+		});
+	}
+
+	private static Map<String, String> getFieldsFromPdfForm(File pdfFile) throws InvalidPasswordException, IOException {
+		Map<String, String> propertyMap = new HashMap<>();
+		PDDocument pdDoc = PDDocument.load(pdfFile);
+		PDDocumentCatalog pdCatalog = pdDoc.getDocumentCatalog();
+		PDAcroForm pdAcroForm = pdCatalog.getAcroForm();
+
+		for (PDField pdField : pdAcroForm.getFields()) {
+			propertyMap.put(pdField.getFullyQualifiedName(), pdField.getValueAsString());
 		}
+		return propertyMap;
 	}
 }
